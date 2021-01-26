6547df2788171ae412b2f01c435dd1262b6da8ae
Nithin <nithingudla23@gmail.com>
2021-01-26 13:30:39 +0530

    ADD : max function

---
+++ src/max.js
@@ -0,0 +1,12 @@
+function recursion(index, list){
+
+    if(index == list.length - 1)
+        return list[index];
+    let temp = recursion(index + 1, list)
+    return list[index] > temp ? list[index] : temp;
+}
+
+const max = (list) => {
+    return recursion(0, list)   
+}
+module.exports = max;
